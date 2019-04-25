1d50529f5780d8bd70571903166827d61af63e03
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-04-25 08:36:31 +0700

    'Change Direction'

--- ball.js
+++ ball.js
@@ -37,13 +37,8 @@ let Ball = function () {
   this.hitPaddle = function () {
     if ((self.y > (paddle.y - self.radius - self.yVelocity)) && (self.x >= paddle.x) && (self.x <= paddle.x + paddle.width)) {
       let dist = self.x - (paddle.x + paddle.width/2);
-      xVelocityBefore = self.xVelocity
-      reverseX = self.xVelocity* 2 * dist/paddle.width * (self.xVelocity===0?0:self.xVelocity<0?-1:1)
-      calculateX = Math.sqrt(self.velocityMagnitude* self.velocityMagnitude)*dist/paddle.width -(self.xVelocity/self.velocityMagnitude)*Math.sqrt(self.velocityMagnitude* self.velocityMagnitude)*(-dist/paddle.width)
       self.xVelocity = self.xVelocity* 2 * dist/paddle.width * (self.xVelocity===0?0:self.xVelocity<0?-1:1) + (1-Math.abs(self.xVelocity/self.velocityMagnitude))*Math.sqrt(self.velocityMagnitude* self.velocityMagnitude)*dist/paddle.width;
-      xVelocityAfter = self.xVelocity
       self.yVelocity = - Math.sqrt(self.velocityMagnitude*self.velocityMagnitude-self.xVelocity*self.xVelocity)
-      yVelocityAfter = self.yVelocity
     }
   }
   this.hitConnerOfPaddle = function () {
